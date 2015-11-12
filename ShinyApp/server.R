020f1c7899c20b8ea7d7cd3d1218a35a3c27561d
poball <poball@gmail.com>
2015-11-12 15:23:04 -0500

    Create server.R

---
+++ ShinyApp/server.R
@@ -0,0 +1,53 @@
+
+library(shiny)
+library(datasets)
+attach(mtcars)
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+shinyServer(function(input, output) {
+  
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  formulaTextPoint <- reactive({
+    paste("mpg ~", "as.integer(", input$variable, ")")
+  })
+  
+  fit <- reactive({
+    lm(as.formula(formulaTextPoint()), data=mpgData)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  output$mpgBoxPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData,
+            outline = input$outliers)
+  })
+  output$scatterplot <- renderPlot({
+    pairs(mpgData,panel = panel.smooth)
+  })
+  
+  output$fit <- renderPrint({
+    summary(fit())
+  })
+  
+  output$mpgPlot <- renderPlot({
+    with(mpgData, {
+      plot(as.formula(formulaTextPoint()))
+      abline(fit(), col=2)
+    })
+  })
+  
+  # subset dataset
+  output$dTable <- renderDataTable({
+    subset(mpgData, mpg>=input$MPG[1] & mpg<=input$MPG[2])
+           
+  })
+
+})
